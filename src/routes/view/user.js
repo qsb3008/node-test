5bfbeb5ba828eee7141212019ec06d9931f4df40
qsb3008 <qsb3008@163.com>
2020-03-23 10:18:10 +0800

    fix: 判断前端登录状态

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -1,15 +1,26 @@
 const router = require('koa-router')()
 
-router.get('/login', async (ctx, next) => {
-  await ctx.render('login', {
+// 查看是否登录，避免重复登录
+function getLoginInfo (ctx) {
+  let data = {
+    isLogin: false // 默认未登录
+  }
+  const userInfo = ctx.session.userInfo
+  if (userInfo) {
+    data = {
+      isLogin: true,
+      userName: userInfo.userName
+    }
+  }
+  return data
+}
 
-  })
+router.get('/login', async (ctx, next) => {
+  await ctx.render('login', getLoginInfo(ctx))
 })
 
 router.get('/register', async (ctx, next) => {
-  await ctx.render('register', {
-    
-  })
+  await ctx.render('register',getLoginInfo(ctx))
 })
 
 module.exports = router
