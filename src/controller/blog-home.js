892f2095467d787a22dd517a0575b8d95c4c2ab2
qsb3008 <qsb3008@163.com>
2020-04-02 10:36:04 +0800

    test: 测试创建微博

--- src/controller/blog-home.js
+++ src/controller/blog-home.js
@@ -6,7 +6,7 @@
 const { createBlog } = require('../services/blog')
 const { SuccessModel, ErrorModel } = require('../model/ResModel')
 const { createBlogFailInfo } = require('../model/ErrorInfo')
-
+const xss = require('xss')
 /**
  * 创建微博
  * @param {Object} param0 创建微博所需的数据 { userId, content, image }
@@ -16,7 +16,7 @@ async function create({ userId, content, image }) {
         // 创建微博
         const blog = await createBlog({
             userId,
-            content,
+            content: xss(content),
             image
         })
         return new SuccessModel(blog)
---
+++ test/testUserInfo.js
@@ -0,0 +1,13 @@
+/**
+ * @description 单元测试的用户信息
+ * @author 双越老师
+ */
+
+/**
+ * 【特别提醒】cookie 是用户的敏感信息，此处只能是**测试**用户的 cookie
+ * 每次测试用户重新登录，都需要更新这里的 cookie
+ */
+
+module.exports = {
+    COOKIE: 'weibo.sid=bXwJYphwF3yuDThmi1Em5YC1l8RTM6vo; weibo.sid.sig=xw2He3sT1kKChxdhSmxSBXqgNeU'
+}
